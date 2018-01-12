683176ff17456ac9d14c11ded28c91f909ca0970
Auston Stewart <auston@shedbuilt.net>
2018-01-12 00:16:09 -0500

    Adding the toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-./configure --prefix=/usr
+case "$SHED_BUILDMODE" in
+    toolchain)
+        SHEDPKG_PREFIX='/tools'
+        ;;
+    *)
+        SHEDPKG_PREFIX='/usr'
+        ;;
+esac
+./configure --prefix=$SHEDPKG_PREFIX
 make -j $SHED_NUMJOBS
-make DESTDIR=$SHED_FAKEROOT install
+make DESTDIR="$SHED_FAKEROOT" install
